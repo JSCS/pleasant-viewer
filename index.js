a06a3687bfb628ff5fa92f10ced334d5deb3960f
Julian <Julian.Schwartz@gmail.com>
2013-06-01 02:30:04 -0700

    live coloring and scroll

--- index.js
+++ index.js
@@ -29,23 +29,31 @@ $j(function(){
   function current_line(){
     return $j('#citations').val().substr(0, $j('#citations')[0].selectionStart).split("\n").length;
   }
+  function scroll_preview(){
+    $j('#sortable').scrollTop($j('#sortable').scrollTop() + ($j('#x' + current_line()).position().top - $j('#sortable').position().top) - ($j('#sortable').height()/2) + ($j('#x' + current_line()).height()/2) );
+  }
   
   function update_preview(){
     if ($j('#x' + current_line()).length == 0) {
       	$j('#sortable').append('<dl id="x'+current_line()+'">      	  <dt id="citation_citation_'+current_line()+'">Citation #'+current_line()+'</dt>	  <dd id="citation_text_'+current_line()+'">Please type eg 4:4 or Gen 1:1...</dd> </dl>');      
     }
+    $j('#sortable > dl').each(function () {this.style.cssText = 'color: black;'});
+    $j('#x' + current_line()).attr('style','color: blue;');
+    scroll_preview();
     reload_citation();
   }
+  
   function reload_citation(){
     $j('#tooltip').text("Busy, keep typing..");    
     console.log(current_line());    
-    
-      if (!isNaN($j('#citations').val().charAt(0))) {
-	$j('#detected_book').text('S&H');    
+    cl = current_line();
+    cit = $j('#citations').val().split('\n')[cl-1];
+      if (!isNaN(cit.charAt(0))) {
+	$j('#detected_book').text('  Book: S&H');    
       }
       else {
-	$j('#detected_book').text('KJV');    
-      }	    
+	$j('#detected_book').text('  Book: KJV');    
+      }	        
     cl = current_line();
     cit = $j('#citations').val().split('\n')[cl-1];
     console.log($j('#citations').val().split('\n')[0]);
@@ -57,7 +65,9 @@ $j(function(){
       $j('#citation_citation_'+cl).text(citation.citation);
       $j('#citation_text_'+cl).text(citation.text);  
       $j('#tooltip').text("Done, hit enter for the next one..");
+      scroll_preview();
     });
+    
   }
 
   $j('#citations').change(update_preview);
--- template_entry_form.php
+++ template_entry_form.php
@@ -12,16 +12,14 @@
 
     
     <div style="clear: both; width: 30%; float: left; margin-top: 30px; overflow:auto;">    
-      <strong>Citations</strong><br /><span id="tooltip">Type your citation and hit <b>enter</b>..</span>
+      <strong>Citations</strong><span id="detected_book">Book</span><br /><span id="tooltip">Type your citation and hit <b>enter</b>..</span>
       <textarea name="post_citations" id="citations" rows="16" cols="200"><?php echo strip_tags(stripslashes($_POST['post_citations'])); ?></textarea>
     </div>
 
         
     <div style="width: 65%; float: right; margin-top: 30px;">    
-      <strong>Preview</strong><span id="detected_book">Book</span><br />
-        <div id="sortable">
-	  <dl id="cite" class="ui-state-default">      	
-	  </dl>
+      <strong>Preview</strong><br />
+        <div id="sortable" style="height: 500px;overflow: auto;">	  
 	</div>		
     </div>
 
